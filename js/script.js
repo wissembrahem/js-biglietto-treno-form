57d97723d2440a6d9096be20f55cb7e8d6d2f6fa
Wissembrahem <wbrahem19@gmail.com>
2025-11-06 16:17:08 +0100

    esercizio

--- js/script.js
+++ js/script.js
@@ -6,13 +6,27 @@
 //il prezzo del biglietto è definito in base ai km(0.21 € al km),
 //    va applicato uno sconto del 20 % per i minorenni,
 //        va applicato uno sconto del 40 % per gli over 65.,
-//
-//            MILESTONE 1:
-//Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone(non stilizzati), realizziamo le specifiche scritte sopra.La risposta finale(o output) sarà anch’essa da scrivere in console.
-//
-//    MILESTONE 2:
-//Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. 
-///Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). 
-//
-//Nota:
-//Se non vi sentite particolarmente creativi, questa potrebbe essere un’implementazione da seguire per il secondo milestone.Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma in ogni caso cercate di farla vostra.
+//     IMPOSTAZIONE DI PENSIERO 
+//_X_prima fase creare il programma di calcolo per il biglietto 
+//_X_impostare gli sconti 
+//_ _ iniziare il programma senza alcuna estetica tramite due input e un bottone 
+//_X_ impostare l'output
+
+
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', function() {
+    const manyKm  = document.getElementById('manyKm');
+    const userAge = document.getElementById('userAge');
+    let ticketPrice = manyKm.value * 0.21;
+    if (userAge.value < 18) {
+        ticketPrice = ticketPrice * 0.8;
+    } else if (userAge.value > 65) {
+        ticketPrice = ticketPrice *0.6
+    }
+
+    const results = document.getElementById('results');
+
+    // output
+   results.innerHTML = ticketPrice;
+});
